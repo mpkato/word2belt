c8f1b8d2d0b8efb0cbb897a0e1518cb1f106dbdf
mpkato <kato@dl.kuis.kyoto-u.ac.jp>
2016-10-17 14:21:47 +0900

    First commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        pytest.main(self.test_args)
+
+setup(
+    name = "word2belt",
+    packages = ["word2belt"],
+    version = "0.0.1",
+    description = "Word2Belt",
+    author = "Makoto P. Kato",
+    author_email = "kato@dl.kuis.kyoto-u.ac.jp",
+    license     = "MIT License",
+    url = "https://github.com/mpkato/word2belt",
+    setup_requires = [],
+    install_requires = [
+        'requests'
+    ],
+    tests_require=['pytest'],
+    cmdclass = {'test': PyTest}
+)
